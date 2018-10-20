0bab68ec8735823948c939920aa2080e9cbbf6ca
Funmi8975 <34584846+Funmi8975@users.noreply.github.com>
2018-10-20 16:29:46 -0500

    Create New York Times API. swipe-based interface.cpp

---
+++ New York Times API. swipe-based interface.cpp
@@ -0,0 +1,386 @@
+<!-- Create a client for the New York Times API. swipe-based interface -->
+
+<!--HTML FILE -->
+
+<html>
+<head>
+  <link rel="stylesheet" type="text/css" href="main.css">
+  <script async type="text/javascript" src="main.js"></script>
+</head>
+  
+  <body>
+    <!-- The navigation links at the top of the page  -->
+ <div class="topnavigation">
+  <a href="#home" target="blank">Home</a>
+  <a href="#news" target="blank">News</a>
+  <a href="#about" target="blank">About</a>
+  <a href="#contact"target="blank">Contact Us</a>
+  <input type="text" placeholder="Search...">
+ </div>
+    <header>
+  <h1>New York Times</h1>
+      <h2>Top Stories</h2>
+    </header>
+    
+    <!-- List of the grid cells --> 
+ <ul id="grid">
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Opinions</span>
+        </a>
+    </li>
+     <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Politics</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="" target="blank">
+              <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Upshots</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Technology</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Science</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">World</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Insider</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Business</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">nyreligion</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">National</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Sports</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Health</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Insider</span>
+        </a>
+    </li>
+  <li>
+  <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Opinion</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Arts</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Books</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Movies</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Theaters</span>
+        </a>
+    </li>
+    <li><a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Sundayreview</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Fashion</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="rig-text">Tmagazine</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Food</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Travel</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#" target="">
+           <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Magazine</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#" target="">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Real-Estate</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#" target="">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Automobiles</span>
+        </a>
+    </li>
+    <li>
+        <a class="grid-cell" href="#" target="">
+            <img class="grid-img" src="345">
+            <span class="grid-overlay"></span>
+            <span class="grid-text">Obituaries</span>
+        </a>
+    </li> 
+</ul>    
+<footer>
+  <p> All rights reserved; </p>
+</footer>
+</body>
+</html>
+
+
+
+
+
+
+
+<!-- CSS FILE -->
+
+/*  top navigation bar's background color */
+ .topnavigation {
+  overflow: hidden;
+  background-color: blue;
+ }
+/* links inside the navigation bar */
+ .topnavigation a { 
+  float: left;
+  display: block;
+  color: yellow;
+  text-align: center;
+  padding: 6px 8px;
+  text-decoration: none;
+  font-size: 20px;
+ }
+/*  links' color on hovering */
+ .topnavigation a:hover {
+  background-color: f8f8f8;
+  color: red;
+ }
+/* Active element to highlight the current page */
+ .topnavigation a.active {
+  background-color: blue;
+  color: red;
+ }
+/* Search box inside the top navigation bar */
+ .topnavigation input[type=text] {
+  float: right;
+  padding: 9px;
+  margin-top: 11px;
+  margin-right: 20px;
+  border: none;
+  font-size: 12px;
+ }
+  .topnavigation input[type=text] {
+   border: 5px solid #f26;
+  }
+ }
+/*Header */
+header {
+ background-color: #564;
+ padding: 20px;
+ text-align: center;
+ font-size: 40px;
+ color: white;
+}
+/* Footer */
+footer {
+ background-color: white;
+ padding: 14px;
+ text-align: center;
+ color: black;
+}
+/* Image grid */
+#grid {
+    max-width:800px;
+    margin:0 auto; /*center aligned grid*/
+    padding:0;
+    font-size:12; 
+    list-style:none;
+    background-color:white;
+}
+#grid li {
+    display: inline-block;
+    *display:inline;
+    width:28%;
+    vertical-align:middle;
+    box-sizing:border-box;
+    margin:0;
+    padding:0;
+}
+        
+/* wrapper for each image */
+.grid-cell {
+    display:block;
+    position: relative;
+    overflow:hidden;
+}
+       
+.grid-img {
+    display:block;
+    width: 100%;
+    height: auto;
+    border:none;
+    transform:scale(1);
+    transition:all 1s;
+}
+#grid li:hover .grid-img {
+    transform:scale(1.05);
+}
+        
+/* grid overlay layer */
+.grid-overlay {
+    position: absolute;
+    display:block;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    margin: auto;
+    background: #3DC0F1 url(img/link.png) no-repeat center 20%;
+    background-size:45px 45px;
+    opacity:0;
+    filter:alpha(opacity=0);
+  
+}
+#grid li:hover .grid-overlay {
+    opacity:0.8;
+}
+/* If  captions exist*/
+.grid-text {
+    display:block;
+    padding:0 40px;
+    box-sizing:border-box;
+    position:absolute;
+    left:0;
+    width:100%;
+    text-align:center;
+    text-transform:capitalize;
+    font-size:12px;
+    font-weight:bold;
+    font-family: 'Algerian', sans-serif;
+    font-weight:normal!important;
+    top:40%;
+    color:red;
+    opacity:0;
+    filter:alpha(opacity=0);
+    transform:translateY(-20px);
+    transition:all .3s;
+}
+#grid li:hover .grid-text {
+    transform:translateY(0px);
+    opacity:0.9;
+}
+@media (max-width: 7000px) {
+    #grid li {
+        width:25%;
+    }
+}
+#grid li {
+        width:33.33%;
+    }
+}
+@media (max-width: 600px) {
+    #grid li {
+        width:50%;
+    }@media (max-width: 700px) {
+} */
+
+<!-- JavaScript -->
+
+<script
+
+<!-- API KEY -->
+
+</script>
+
